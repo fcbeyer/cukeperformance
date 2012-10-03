21a543e4418deb46156ba9aa96f20765eddf012a
Rick Beyer <rbeyer@patientkeeper.com>
2012-10-03 15:49:46 -0400

    first commit for new project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CukePerformance::Application.config.secret_token = '95d147e16b270c256044932e7ea696bfa8fbfb49a3c30f72ecce4bee27a2fedf71b2138ec9ae3d1c9b0ca9abc8de3fc29dc1b931f6d1d58d5791e6a6ab714159'
