aca2fb3df8dcfb5b7f8b23e9edd6a984172eee6a
Rick Beyer <rbeyer@patientkeeper.com>
2012-10-05 15:31:55 -0400

    purging the rest of bvt files

--- app/models/bvt.rb
+++
@@ -1,9 +0,0 @@
-class Bvt < ActiveRecord::Base
-	#date and time need to be unique but only when strung together?
-	def getFullName(bvt_id)
-		bvt_build = Bvt.find(bvt_id)
-		fullName = bvt_build.build_date + "_" + bvt_build.build_time
-		return fullName
-	end
-	
-end
