93cbe404e725a1147dbd68b56eee436fd7e2b627
Rick Beyer <rbeyer@patientkeeper.com>
2012-11-21 11:12:28 -0500

    made Feature graphing working, but only shows BVTs currently

--- app/controllers/graph_controller.rb
+++ app/controllers/graph_controller.rb
@@ -4,7 +4,14 @@ class GraphController < ApplicationController
 		@suites.sort! { |a,b| a.runstamp <=> b.runstamp}
   end
 
-  def features
+  def features		
+		# @features = Feature.find_by_sql("select f.name, f.duration, f.status, f.duration_converted, s.runstamp, s.build_date, s.build_time " +
+																		# "from Features f " +
+																		# "inner join Suites s on s.id = f.suite_id " +
+																		# "where s.name = 'BVT'")
+			#this is the equivalent of the above SQL statement
+			@features = Feature.joins(:suite).where(:suites => {:name => 'BVT'}).select("features.*,suites.runstamp,suites.build_time,suites.build_date")
+			@features.sort! { |a,b| a.runstamp <=> b.runstamp}
   end
 
   def scenarios
