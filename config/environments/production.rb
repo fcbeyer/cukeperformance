0811b65e932a2fd6dc07b81c59161a645a38adc3
Rick Beyer <rbeyer@patientkeeper.com>
2013-03-15 15:00:59 -0400

    fixing more prod stuff

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,4 +67,12 @@ CukePerformance::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+  ActionMailer::Base.delivery_method = :smtp
+  ActionMailer::Base.perform_deliveries = true
+  ActionMailer::Base.raise_delivery_errors = true
+  ActionMailer::Base.smtp_settings = {
+    :address            => 'exchange.ne.virtmed.com',
+    :port               => 25,
+    :domain             => 'virtmed'
+  }
 end
