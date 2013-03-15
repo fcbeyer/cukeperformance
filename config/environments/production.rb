fec1aac98f1642d06e725d56d98e2428a8e12b01
Rick Beyer <rbeyer@patientkeeper.com>
2013-03-15 14:56:04 -0400

    adding to prod file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,6 +47,9 @@ CukePerformance::Application.configure do
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
+  
+  # where to direct Users when they receive an email
+  config.action_mailer.default_url_options = { :host => 'pickle:8980' }
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
