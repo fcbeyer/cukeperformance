5b0a0f10f822fe143bef506faf5386b0cdf3e21c
Rick Beyer <rbeyer@patientkeeper.com>
2013-11-07 11:16:34 -0500

    updated script to handle date time being passed in

--- script/auto_update.rb
+++ script/auto_update.rb
@@ -1,19 +1,25 @@
 require 'getoptlong'
 
-def auto_create_dub(cuke_path,cuke_name,cuke_file)
+def auto_create_dub(cuke_path,cuke_name,cuke_file,cuke_time)
 	send_email = true
 	cuke_results = CukeParser.json_complete(cuke_path,cuke_file,"systemData")
-	puts cuke_results.converted_duration
-	puts cuke_results.browser
+	date = cuke_time.split(" ")[0]
+	time = cuke_time.split(" ")[1]
+	time.gsub!(":","-")
+	puts cuke_results.date
+	puts cuke_results.time
 	puts cuke_results.url
 	file = File.new("results.txt","w+")
 	CukeParser.write_pretty(cuke_results,file)
 end
 
-def auto_create(cuke_path,cuke_name,cuke_file)
+def auto_create(cuke_path,cuke_name,cuke_file,cuke_time)
 	send_email = true
+	date = cuke_time.split(" ")[0]
+	time = cuke_time.split(" ")[1]
+	time.gsub!(":","-")
 	cuke_results = CukeParser.json_complete(cuke_path,cuke_file,"systemData")
-	suite = Suite.new({:build_date => cuke_results.date,:build_time => cuke_results.time,:runstamp => cuke_results.runstamp,:duration => cuke_results.duration,
+	suite = Suite.new({:build_date => date,:build_time => time,:runstamp => cuke_time,:duration => cuke_results.duration,
 						:duration_converted => cuke_results.converted_duration,:browser => cuke_results.browser,:os => cuke_results.os,:mobilizer => cuke_results.branch_number,
 						:mobilizer_build_tag => cuke_results.branch_build_tag,:url => cuke_results.url,:name => cuke_name,:status => cuke_results.status,:exclude => false})
 	suite.save
@@ -42,15 +48,18 @@ def setup_data
 	opts = GetoptLong.new(
 												['--cuke_path', GetoptLong::REQUIRED_ARGUMENT],
 												['--cuke_file', GetoptLong::REQUIRED_ARGUMENT],
-												['--cuke_name', GetoptLong::REQUIRED_ARGUMENT])
+												['--cuke_name', GetoptLong::REQUIRED_ARGUMENT],
+												['--cuke_time', GetoptLong::REQUIRED_ARGUMENT])
 	opts.each do |option, arg|
 		case option
 			when '--cuke_name'
 				options[:cuke_name] = arg
 			when '--cuke_path'
 				options[:cuke_path] = arg
-				when '--cuke_file'
+			when '--cuke_file'
 				options[:cuke_file] = arg
+			when '--cuke_time'
+				options[:cuke_time] = arg
 		end
 	end
 	return options
@@ -59,4 +68,4 @@ end
 ENV['RAILS_ENV'] = "development" # Set to your desired Rails environment name
 require_relative '..\config\environment.rb'
 options = setup_data
-auto_create(options[:cuke_path],options[:cuke_name],options[:cuke_file])
+auto_create(options[:cuke_path],options[:cuke_name],options[:cuke_file],options[:cuke_time])
